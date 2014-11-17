a12d6e0a1dd7796e5baf14b8c6d2321ba4c8f728
Javier <javucho1000@yahoo.com.mx>
2014-11-17 23:50:13 +0900

    Adding Capybara/Rspec integration test

--- sample/integration-test/spec/features/user_signs_in_spec.rb
+++ sample/integration-test/spec/features/user_signs_in_spec.rb
@@ -1,2 +1,12 @@
 require 'capybara/rspec'
 
+describe "sign in", :type => :feature, :js => true do
+
+  it "signs the user in" do
+    visit 'https://browseridverifiersample-user454322.rhcloud.com'
+    click_button 'persona_sign_in_up'
+  end
+
+end
+
+
