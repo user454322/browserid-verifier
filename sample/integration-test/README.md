0f00eaf5bce8fc3c50bcef2658b79748593dbf21
Javier <javucho1000@yahoo.com.mx>
2014-11-20 01:23:11 +0900

    Work on integration tests

---
+++ sample/integration-test/README.md
@@ -0,0 +1,13 @@
+
+The integration tests are specified using Rspec.
+These test also need Selenium and Capybara, so Ruby (YARV) 2.1.2 and 
+the following Gems are required:
+ * rspec -v 3.1.0
+ * capybara -v 2.4.4
+ * capybara -v 2.4.4
+ * selenium-webdriver -v 2.44.0
+ 
+
+To run the tests: 
+`rspec spec/features/user_signs_in_spec.rb`
+
--- sample/integration-test/spec/features/user_signs_in_spec.rb
+++ sample/integration-test/spec/features/user_signs_in_spec.rb
@@ -1,11 +1,28 @@
 require 'capybara/rspec'
+require 'yaml'
 
 describe "sign in", :type => :feature, :js => true do
-
-  it "signs the user in" do
-    visit 'https://browseridverifiersample-user454322.rhcloud.com'
-    click_button 'persona_sign_in_up'
+  before(:all) do
+    puts "Nanoni"
+    config = YAML.load_file('config.yaml')
+    @email = config['authentication_email']
+    @passwd = config['authentication_password']
   end
 
+  it "signs in the user using Mozilla's Persona" do
+    visit 'https://browseridverifiersample-user454322.rhcloud.com'
+    persona_window = window_opened_by do
+      click_button 'persona_sign_in_up'
+    end
+    within_window persona_window do
+      Capybara.default_wait_time = 10
+      puts @email
+      find_by_id('authentication_email').set(@email)
+      find(:xpath, '//*[@id="authentication_form"]/p[4]/button[1]').click
+      find_by_id('authentication_password').set(@passwd)
+      find(:xpath, '//*[@id="authentication_form"]/p[4]/button[3]').click
+    end
+  end
 end
+#https://github.com/jnicklas/capybara
 
